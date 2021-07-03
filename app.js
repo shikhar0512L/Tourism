dab8fd97b97138d7b5899d1b2e93b24a0a0afb27
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2021-07-03 13:21:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log(`Welcome to our website`);
+// Creating some constants and variable...
+const date = new Date();
+const link = document.getElementsByClassName(`link`);
+const Rajasthan = document.getElementsByClassName(` Rajasthan`);
+const Kashmir = document.getElementsByClassName(` Kashmir`);
+
+
+// Printing all to the console to see that  if is it working or not..
+console.log(date);
+console.log(Rajasthan);
+console.log(link);
+console.log(Kashmir);
---
+++ form.js
@@ -0,0 +1,13 @@
+console.log('Welcome to forms section');
+
+// IMPORTANT
+function hideFunction() {
+    var alert = document.getElementById("alert");
+    alert.style.display = "block"
+    return true;
+};
+
+const dt = new Date();
+console.log(dt);
+
+
